52a5ecba49eff225a668ca0c5447d1b413668857
KauanyBeatriz <110268844+KauanyBeatriz@users.noreply.github.com>
2023-06-01 20:53:24 -0300

    Versão 1
    Atualização

---
+++ Flask versão 1/Flask/main.py
@@ -0,0 +1,6 @@
+# Importando o app
+from divtechne import app
+
+# Se esse arquivo está a ser executado ele mesmo
+if __name__ == "__main__":
+        app.run(debug=True, port='8888') # Alterações serão implementadas sem parar de rodar
